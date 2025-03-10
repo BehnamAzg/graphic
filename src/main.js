bdb673011f6ea7bb3ce80b92c4d866ddf1e220e9
Behnam <BehnamAzg@gmail.com>
2025-03-10 13:31:48 +0330

    video patch 3

--- src/main.js
+++ src/main.js
@@ -57,7 +57,7 @@ POSTS.forEach(function (post) {
     postHTML = `
     <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col justify-end posts" data-id="${post.id}">
       <button class="rounded-md overflow-hidden hover:scale-105 hover:-translate-y-3 transition-all hover:shadow-md relative bg-white aspect-square">
-        <video class="h-full w-full image" src="${post.video}" autoplay loop muted></video>
+        <video class="h-full w-full image" src="${post.video}" muted autoplay playsinline loop ></video>
         <div class="w-full h-full absolute inset-0 flex justify-center items-center loading-spinner">
           <img class=" w-12 h-12" src="./images/preload.svg" />
         </div>
@@ -91,3 +91,10 @@ POSTS.forEach(function (post) {
 
 // show copyright
 document.getElementById("copyright").innerHTML = `&copy; ${new Date().getFullYear()} Behnam Azg, All rights reserved.`;
+
+// video auto play
+document.addEventListener("click", function () {
+  document.querySelectorAll("video").forEach((video) => {
+    video.play().catch((e) => console.log("Autoplay still blocked:", e));
+  });
+});
