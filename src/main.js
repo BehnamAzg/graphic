d2b8081da3901ac85a1bd5bff045d77e105c25e1
Behnam <BehnamAzg@gmail.com>
2025-03-10 13:05:13 +0330

    test patch for video support

--- src/main.js
+++ src/main.js
@@ -32,11 +32,11 @@ document.addEventListener("DOMContentLoaded", () => {
       const container = media.parentElement;
       container.querySelector(".loading-spinner").style.display = "none";
     };
-    // for videos
-    media.addEventListener("loadeddata", () => {
-      const container = media.parentElement;
-      container.querySelector(".loading-spinner").style.display = "none";
-    });
+    // // for videos
+    // media.addEventListener("loadeddata", () => {
+    //   const container = media.parentElement;
+    //   container.querySelector(".loading-spinner").style.display = "none";
+    // });
   });
 });
 
@@ -57,10 +57,10 @@ POSTS.forEach(function (post) {
     postHTML = `
     <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col justify-end posts" data-id="${post.id}">
       <button class="rounded-md overflow-hidden hover:scale-105 hover:-translate-y-3 transition-all hover:shadow-md relative bg-white aspect-square">
-        <video class="h-full w-full image" src="${post.video}" loading="lazy" autoplay loop muted></video>
-        <div class="w-full h-full absolute inset-0 flex justify-center items-center loading-spinner">
+        <video class="h-full w-full image" src="${post.video}" autoplay loop muted></video>
+        <!-- <div class="w-full h-full absolute inset-0 flex justify-center items-center loading-spinner">
           <img class=" w-12 h-12" src="./images/preload.svg" />
-        </div>
+        </div> -->
       </button>
         <div class="pt-3 flex items-center justify-between px-1">
           <p class="text-sm text-gray-400">${post.categorie}</p>
